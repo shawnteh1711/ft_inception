d4822fd522ab39e76037d195cd874d8c811255aa
Shawn Teh <steh@u90z01s01.42kl.edu.my>
2023-02-16 20:52:52 +0800

    update file

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -3,13 +3,13 @@
 # Check if there is mysql default files
 # if [ ! -d "/var/lib/mysql/mysql/" ]; then
     # initializa sql database and specifies the location where data files will be stored
-    mysql_install_db --datadir=/var/lib/mysql
+    mysql_upgrade --datadir=/var/lib/mysql
+    # mysql_install_db --datadir=/var/lib/mysql
     chown -R mysql:root /var/lib/mysql
 
     # Create bootstrap file
     echo 
-    "
-    USE mysql;
+    " USE mysql;
     FLUSH PRIVILEGES;
     CREATE USER IF NOT EXISTS '$MYSQL_ADMIN'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';
     CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
@@ -18,8 +18,7 @@
     GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
     SELECT user from mysql.user;
     ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-    FLUSH PRIVILEGES;
-    " > temp.sql
+    FLUSH PRIVILEGES;" > temp.sql
 
     # Insert boostrap file into db as mysql user using mysqld daemon to run mysql server. 
     # A daemon is a program that runs in background and provide some service for other programs or user.
