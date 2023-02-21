3675ce4b5ab898454bf89a93544a9e267415b6df
Shawn Teh <steh@u90z01s02.42kl.edu.my>
2023-02-21 21:32:24 +0800

    update

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -14,11 +14,22 @@ if [ ! -f ./wp-config.php ]; then
     # Remove the archive and wordpress files
     rm -rf latest.tar.gz && rm -rf wordpress
 
-    sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-    sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-    sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-    sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-    cp wp-config-sample.php wp-config.php
+    # Download wp core files
+    wp core download --allow-root
+
+    # Configure wp
+    wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${$MYSQL_HOSTNAME} --allow-root 
+
+    # Install wp
+    wp core install --url=localhost --title="My blog" --admin_name=$MYSQL_ADMIN --admin_password=$MYSQL_ADMIN_PASSWORD --admin_email=you@example.com --allow-root
+
+
+
+    # sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+    # sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+    # sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+    # sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+    # cp wp-config-sample.php wp-config.php
 
 else
     echo "Wordpress has already been installed"
