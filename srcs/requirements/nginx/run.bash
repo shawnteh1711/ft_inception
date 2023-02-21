928d1455182fc5c46bddeb5d7a5da34ca0e43301
Shawn Teh <steh@u90z01s02.42kl.edu.my>
2023-02-21 10:59:15 +0800

    update

--- srcs/requirements/nginx/run.bash
+++ srcs/requirements/nginx/run.bash
@@ -2,5 +2,5 @@ git pull
 docker build -t nginx .
 docker stop nginxc
 docker rm nginxc
-docker run -d --name -p 443:443 nginxc nginx
+docker run -d --name nginxc -p 443:443 nginx
 docker exec -it nginxc bash
