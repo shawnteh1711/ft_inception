c190fa12aca6c00e65e6034df976a2fc0b27dcab
Shawn Teh <steh@u90z03s02.42kl.edu.my>
2023-02-22 13:17:33 +0800

    update

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -23,11 +23,11 @@ if [ ! -f ./wp-config.php ]; then
     # Install wp
     wp core install --url=localhost --title="My blog" --admin_name=$MYSQL_ADMIN --admin_password=$MYSQL_ADMIN_PASSWORD --admin_email=you@example.com --allow-root
 
-    # sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-    # sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-    # sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-    # sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-    # cp wp-config-sample.php wp-config.php
+    sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+    sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+    sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+    sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+    cp wp-config-sample.php wp-config.php
 else
     echo "Wordpress has already been installed"
 fi
