8673a176ef0c7e9b9db4fc4d282141a0db5df430
Shawn Teh <steh@u90z03s02.42kl.edu.my>
2023-02-22 14:00:40 +0800

    update

--- srcs/requirements/nginx/tools/ssl.sh
+++ srcs/requirements/nginx/tools/ssl.sh
@@ -9,7 +9,7 @@ mkdir -p $SSL_DIR
 # Generate a new private key and self-signed SSL certificate using OpenSSL
 openssl req -x509 -nodes -newkey rsa:4096 \
     -keyout $SSL_DIR/shawn.key -out $SSL_DIR/shawn.crt \
-        -days 365 -subj "/C=MY/ST=Selangor/L=PetalingJaya/0=42KL/OU=42KL/CN=shawn"
+        -days 365 -subj "/C=MY/ST=Selangor/L=PetalingJaya/O=42KL/OU=42KL/CN=shawn"
 
 
 # Run the command passed as an argument to the script
