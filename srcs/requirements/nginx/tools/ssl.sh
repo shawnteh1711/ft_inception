8e36ab8495535df9fdff4672b20f6a4ad0c8dc80
bocal <bocal@u91z01s05.42kl.edu.my>
2023-03-01 07:26:16 +0800

    update

--- srcs/requirements/nginx/tools/ssl.sh
+++ srcs/requirements/nginx/tools/ssl.sh
@@ -1,27 +1,16 @@
-# #!/bin/sh
+#!/bin/sh
 
-# # Set the path where SSL certificate and key will be stored
-# SSL_DIR="/etc/nginx/ssl"
+# Set the path where SSL certificate and key will be stored
+SSL_DIR="/etc/nginx/ssl"
 
-# # Create the SSL directory if it doesn't already exist
-# mkdir -p $SSL_DIR
+# Create the SSL directory if it doesn't already exist
+mkdir -p $SSL_DIR
 
-# # Generate a new private key and self-signed SSL certificate using OpenSSL
-# openssl req -x509 -nodes -newkey rsa:4096 \
-#     -keyout $SSL_DIR/shawn.key -out $SSL_DIR/shawn.crt \
-#         -days 365 -subj "/C=MY/ST=Selangor/L=PetalingJaya/O=42KL/OU=42KL/CN=shawn"
-
-
-# # Run the command passed as an argument to the script
-# exec "$@"
+# Generate a new private key and self-signed SSL certificate using OpenSSL
+openssl req -x509 -nodes -newkey rsa:4096 \
+    -keyout $SSL_DIR/shawn.key -out $SSL_DIR/shawn.crt \
+        -days 365 -subj "/C=MY/ST=Selangor/L=PetalingJaya/O=42KL/OU=42KL/CN=shawn"
 
 
-CERTS_PATH="/etc/nginx/certs"
-
-mkdir -p $CERTS_PATH
-openssl genrsa -out $CERTS_PATH/private.key 2048
-openssl req -new -key $CERTS_PATH/private.key -out $CERTS_PATH/csr.csr -subj "/C=MY/ST=KL/L=KL/O=42/OU=42KL/CN=Hiro"
-openssl x509 -req -days 365 -in $CERTS_PATH/csr.csr -signkey $CERTS_PATH/private.key -out $CERTS_PATH/certificate.crt
-
-
+# Run the command passed as an argument to the script
 exec "$@"
