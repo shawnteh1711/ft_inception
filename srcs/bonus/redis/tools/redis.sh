1ac5fed957a3eb46d372b3d4a2d62dca7656b209
bocal <bocal@u91z01s05.42kl.edu.my>
2023-03-01 17:44:19 +0800

    update

--- srcs/bonus/redis/tools/redis.sh
+++ srcs/bonus/redis/tools/redis.sh
@@ -6,10 +6,10 @@ if [! -f "/etc/redis/redis.conf.bak" ]; then
     cp /etc/redis/redis.conf /etc/redis/redis.conf.bak
 
     # comment out the bind for redis to listen on all hosts
-    sed -i "s|bind 127.0.0.1|#bind 127.0.0.1|g" /etc/redis.conf
+    sed -i "s|bind 127.0.0.1|#bind 127.0.0.1|g" /etc/redis/redis.conf
 
     # set maximum memory Redis can use to 2 megabytes
-    sed -i "s|# maxmemory <bytes>|maxmemory 2mb|g" /etc/redis.conf
+    sed -i "s|# maxmemory <bytes>|maxmemory 2mb|g" /etc/redis/redis.conf
 
     # Set the eviction policy to LRU (least recently used) when memory limit reached
     sed -i "s|# maxmemory-policy noeviction|maxmemory-policy allkeys-lru|g" /etc/redis/redis.conf
