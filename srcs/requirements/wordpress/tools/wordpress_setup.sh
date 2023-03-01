7a3778a6b3d3f76e3359494b847c6267f9acef56
bocal <bocal@u91z01s05.42kl.edu.my>
2023-03-01 09:31:54 +0800

    update

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -27,13 +27,13 @@ if [ ! -f ./wp-config.php ]; then
     wp user create $GENERAL_USER $GENERAL_EMAIL --user_pass=$GENERAL_PASSWD --allow-root
 
     # Change configuration directly without using wp cli
-    sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-    sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-    sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-    sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+    # sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+    # sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+    # sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+    # sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 
     # copy wp-config-sample.php into wp-config.php
-    cp wp-config-sample.php wp-config.php
+    # cp wp-config-sample.php wp-config.php
 else
     echo "Wordpress has already been installed"
 fi
