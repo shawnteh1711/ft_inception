ccef85cba72f0c42ac57347e80debf783a17284d
bocal <bocal@u91z01s05.42kl.edu.my>
2023-03-01 08:11:33 +0800

    update

--- srcs/requirements/nginx/tools/ssl.sh
+++ srcs/requirements/nginx/tools/ssl.sh
@@ -1,28 +1,16 @@
-# #!/bin/sh
-
-# # Set the path where SSL certificate and key will be stored
-# SSL_DIR="/etc/nginx/ssl"
-
-# # Create the SSL directory if it doesn't already exist
-# mkdir -p $SSL_DIR
-
-# # Generate a new private key and self-signed SSL certificate using OpenSSL
-# openssl req -x509 -nodes -newkey rsa:4096 \
-#     -keyout $SSL_DIR/shawn.key -out $SSL_DIR/shawn.crt \
-#         -days 365 -subj "/C=MY/ST=Selangor/L=PetalingJaya/O=42KL/OU=42KL/CN=shawn"
-
-
-# # Run the command passed as an argument to the script
-# exec "$@"
-
 #!/bin/sh
 
-CERTS_PATH="/etc/nginx/ssl"
+# Set the path where SSL certificate and key will be stored
+SSL_DIR="/etc/nginx/ssl"
 
-mkdir -p $CERTS_PATH
-openssl genrsa -out $CERTS_PATH/shawn.key 2048
-openssl req -new -key $CERTS_PATH/shawn.key -out $CERTS_PATH/shawn.csr -subj "/C=MY/ST=KL/L=KL/O=42/OU=42KL/CN=Hiro"
-openssl x509 -req -days 365 -in $CERTS_PATH/shawn.csr -signkey $CERTS_PATH/shawn.key -out $CERTS_PATH/shawn.crt
+# Create the SSL directory if it doesn't already exist
+mkdir -p $SSL_DIR
+
+# Generate a new private key and self-signed SSL certificate using OpenSSL
+openssl req -x509 -nodes -newkey rsa:4096 \
+    -keyout $SSL_DIR/shawn.key -out $SSL_DIR/shawn.crt \
+        -days 365 -subj "/C=MY/ST=Selangor/L=PetalingJaya/O=42KL/OU=42KL/CN=shawn"
 
 
+# Run the command passed as an argument to the script
 exec "$@"
