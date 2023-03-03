377095b9fde4c9b845a9177b4c648c09d7438415
bocal <bocal@u91z01s05.42kl.edu.my>
2023-03-03 14:04:11 +0800

    update

--- srcs/requirements/bonus/mailhog/tools/sendmail.sh
+++ srcs/requirements/bonus/mailhog/tools/sendmail.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Start Mailhog service
+mailhog &
+
 # Wait until Mailhog is ready
 until nc -z localhost 8025; do
     echo "Mailhog is not ready yet. Retrying..."
@@ -10,4 +13,7 @@ done
 echo -e "Subject: Test Email\n\nThis is a test emails from mhsendmail" | /usr/local/bin/mhsendmail --smtp-addr=localhost:1025 recipient@example.com
 
 # Add an infinite loop to prevent the container from exiting
-while true; do sleep 1; done
+# while true; do sleep 1; done
+
+# Run command in Dockerfile
+exec "$@"
