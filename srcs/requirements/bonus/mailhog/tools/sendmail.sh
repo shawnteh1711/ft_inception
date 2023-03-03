e0ae26228521216506650986ad173fe964e2679b
bocal <bocal@u91z01s05.42kl.edu.my>
2023-03-03 10:15:18 +0800

    update

--- srcs/requirements/bonus/mailhog/tools/sendmail.sh
+++ srcs/requirements/bonus/mailhog/tools/sendmail.sh
@@ -11,3 +11,6 @@ echo -e "Subject: Test Email\n\nThis is a test email from mhsendmail" | /usr/loc
 
 # Run the command passed as an argument to the script
 # exec "$@"
+
+# Add an infinite loop to prevent the container from exiting
+while true; do sleep 1; done
