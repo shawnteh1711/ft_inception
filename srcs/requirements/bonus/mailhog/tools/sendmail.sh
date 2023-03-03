99f6cc5117990f4f73004dea964c70796688ed66
bocal <bocal@u91z01s05.42kl.edu.my>
2023-03-03 09:34:30 +0800

    update

--- srcs/requirements/bonus/mailhog/tools/sendmail.sh
+++ srcs/requirements/bonus/mailhog/tools/sendmail.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Wait until Mailhog is ready
-until nc -z localhost 8025; do
+until nc -z localhost 1025; do
     echo "Mailhog is not ready yet. Retrying..."
     sleep 1
 done
 
 # Send a test email using mhsendmail
-echo -e "Subject: Test Email\n\nThis is a test email from mhsendmail" | /usr/local/bin/mhsendmail --smtp-addr=localhost:8025 recipient@example.com
+echo -e "Subject: Test Email\n\nThis is a test email from mhsendmail" | /usr/local/bin/mhsendmail --smtp-addr=localhost:1025 recipient@example.com
 
 # Run the command passed as an argument to the script
 exec "$@" &
