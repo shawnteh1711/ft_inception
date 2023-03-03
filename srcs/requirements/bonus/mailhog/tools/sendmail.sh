ac88dd6f8e41f77754a4aa8ff7a7113897327c2e
bocal <bocal@u91z01s05.42kl.edu.my>
2023-03-03 09:38:25 +0800

    update

--- srcs/requirements/bonus/mailhog/tools/sendmail.sh
+++ srcs/requirements/bonus/mailhog/tools/sendmail.sh
@@ -7,7 +7,7 @@ until nc -z localhost 8025; do
 done
 
 # Send a test email using mhsendmail
-echo -e "Subject: Test Email\n\nThis is a test email from mhsendmail" | /usr/local/bin/mhsendmail --smtp-addr=localhost:1025 recipient@example.com
+echo -e "Subject: Test Email\n\nThis is a test email from mhsendmail" | /usr/local/bin/mhsendmail --smtp-addr=localhost:8025 recipient@example.com
 
 # Run the command passed as an argument to the script
 exec "$@" &
