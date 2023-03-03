aa255c348e577ae83591e878f08e3326de78312b
bocal <bocal@u91z01s05.42kl.edu.my>
2023-03-03 15:00:08 +0800

    update

--- srcs/requirements/bonus/mailhog/tools/sendmail.sh
+++ srcs/requirements/bonus/mailhog/tools/sendmail.sh
@@ -18,22 +18,19 @@
 #!/bin/bash
 
 # Start Mailhog in the background and get its process ID
-/usr/local/bin/MailHog & MAILHOG_PID=$!
+# /usr/local/bin/MailHog & MAILHOG_PID=$!
 
-# Wait until Mailhog is ready
-until nc -z localhost 8025; do
-    echo "Mailhog is not ready yet. Retrying..."
-    sleep 1
-done
+# # Wait until Mailhog is ready
+# until nc -z localhost 8025; do
+#     echo "Mailhog is not ready yet. Retrying..."
+#     sleep 1
+# done
 
 # Send a test email using mhsendmail
 echo -e "Subject: Test Email\n\nThis is a test email from mhsendmail" | /usr/local/bin/mhsendmail --smtp-addr=localhost:1025 recipient@example.com
 
-# Wait until Mailhog has received the email
-until curl -s http://localhost:8025/api/v2/messages | grep -q 'Test Email'; do
-    echo "Test email not found in Mailhog. Retrying..."
-    sleep 1
-done
-
 # Kill Mailhog and exit
-kill $MAILHOG_PID
+# kill $MAILHOG_PID
+
+# Sleep for a few seconds to ensure the email is delivered before supervisord terminates the script
+sleep 10
