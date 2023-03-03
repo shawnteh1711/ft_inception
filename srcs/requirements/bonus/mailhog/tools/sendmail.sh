245822520061ca15f888d0735185298b9123e14e
bocal <bocal@u91z01s05.42kl.edu.my>
2023-03-03 14:33:18 +0800

    update

--- srcs/requirements/bonus/mailhog/tools/sendmail.sh
+++ srcs/requirements/bonus/mailhog/tools/sendmail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Mailhog service
-# MailHog &
+/usr/local/bin/MailHog &
 
 # Wait until Mailhog is ready
 until nc -z localhost 8025; do
@@ -13,4 +13,4 @@ done
 echo -e "Subject: Test Email\n\nThis is a test emails from mhsendmail" | /usr/local/bin/mhsendmail --smtp-addr=localhost:1025 recipient@example.com
 
 # Run command in Dockerfile
-# exec "$@"
+exec "$@"
